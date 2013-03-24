a02df73868073f7b526078e77eaa646bc9cffe17
fredefl <frederiklassen@gmail.com>
2013-03-24 12:30:48 +0100

    Added netz instead of ILMerge.

--- Computer Info C#/Computer Info/bin/Release/ILMerge.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-ILMerge.exe /out:ComputerInfo.exe ComputerInfoUnmerged.exe ^
-Newtonsoft.Json.dll ^
-da-DK/ComputerInfoUnmerged.resources.dll ^
-MetroFramework.dll
-pause
---
+++ Computer Info C#/Computer Info/bin/Release/Merge.bat
@@ -0,0 +1,4 @@
+netz\netz.exe -s -z ComputerInfoUnmerged.exe da-DK\ComputerInfoUnmerged.resources.dll MetroFramework.dll Newtonsoft.Json.dll
+move ComputerInfoUnmerged.exe.netz\ComputerInfoUnmerged.exe ComputerInfo.exe
+rd /s /q ComputerInfoUnmerged.exe.netz
+pause
